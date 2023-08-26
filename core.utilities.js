bfd706e94fe91963f9dba13876040fbff59fb959
Piri Aykut <piriaykut@gmail.com>
2023-08-26 17:37:13 +0300

    core util add getFormToObject functişon

--- core.utilities.js
+++ core.utilities.js
@@ -758,6 +758,17 @@ export class CoreUtilities {
             }, 200);
         }
     }
+    getFormToObject(formIDorClass) {
+        var unindexed_array = $(formIDorClass).serializeArray();
+        var indexed_array = {};
+
+        $.map(unindexed_array, function (n, i) {
+            indexed_array[n['name']] = n['value'];
+        });
+
+        return indexed_array;
+    }
+
     getFormToJSON(formIDorClass) {
         var formdata = $(formIDorClass).serializeArray();
         formdata.push({ name: 'detay', value: app.ilan.editor.getData() });
